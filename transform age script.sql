ebfa68a48bd32419302f94f51a2c823d2ae5baf8
amanda-mendonsa <amariemendonsa@gmail.com>
2025-05-15 17:45:58 -0500

    Delete transform age script.sql

--- transform age script.sql
+++
@@ -1,6 +0,0 @@
-SET SQL_SAFE_UPDATES = 0;
-ALTER TABLE foodallergy
-MODIFY COLUMN age_start_years VARCHAR(50);
-UPDATE `foodallergy`
-SET age_start_years = CONCAT(FLOOR(AGE_START_YEARS), ' yrs and ', ROUND((AGE_START_YEARS - FLOOR(AGE_START_YEARS)) * 12), ' months') Where age_start_years > 0; 
-
