59b0f91ba2328b48cf88fb34932118df2a2d32be
amanda-mendonsa <amariemendonsa@gmail.com>
2025-05-15 17:45:47 -0500

    Delete transform age end script.sql

--- transform age end script.sql
+++
@@ -1,5 +0,0 @@
-USE `capstone`;
-ALTER TABLE foodallergy
-MODIFY COLUMN age_end_years VARCHAR(50);
-UPDATE `foodallergy`
-SET age_end_years = CONCAT(FLOOR(AGE_END_YEARS), ' yrs and ', ROUND((AGE_END_YEARS - FLOOR(AGE_END_YEARS)) * 12), ' months') Where age_end_years > 0; 
